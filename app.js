352f1bf913f066a5212ef0614abc4ae24feeddcd
jesQM <jezuquema@gmail.com>
2020-03-18 17:36:49 +0100

    SDI-NodeJS- 6,7 División de las rutas en módulos, peticiones GET y parámetros

--- app.js
+++ app.js
@@ -3,13 +3,8 @@ let app = express();
 
 app.set('port', 8081);
 
-app.get('/usuarios', function(req, res){
-    res.send("ver usuarios");
-});
-
-app.get('/canciones', function(req, res){
-    res.send("ver canciones");
-});
+require("./routes/rusuarios.js")(app);
+require("./routes/rcanciones.js")(app);
 
 app.listen(app.get('port'), function () {
    console.log("server online");
