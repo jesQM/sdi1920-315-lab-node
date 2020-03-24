cf216b781040897d5efda33538833498dace89da
jesQM <jezuquema@gmail.com>
2020-03-24 21:12:40 +0100

    SDI-NodeJS-8.3 - Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ let swig = require('swig');
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
 
 app.use(express.static('public'));
 app.set('port', 8081);
 app.set('db','mongodb://admin:sdi_Iz9@tiendamusica-shard-00-00-xmbgc.mongodb.net:27017,tiendamusica-shard-00-01-xmbgc.mongodb.net:27017,tiendamusica-shard-00-02-xmbgc.mongodb.net:27017/test?ssl=true&replicaSet=tiendamusica-shard-0&authSource=admin&retryWrites=true&w=majority');
 
-require("./routes/rusuarios.js")(app, swig);
-require("./routes/rcanciones.js")(app, swig, mongo);
+require("./routes/rusuarios.js")(app, swig, gestorBD);
+require("./routes/rcanciones.js")(app, swig, gestorBD);
 require("./routes/rautores.js")(app, swig);
 
 app.get('/promo*', function (req, res) {
