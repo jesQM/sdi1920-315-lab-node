efa690011b4f3e4646f2d34db93c3a6e38018163
jesQM <jezuquema@gmail.com>
2020-03-27 10:34:46 +0100

    SDI–session8-Ejercicio-complementario2

--- routes/rcomentarios.js
+++ routes/rcomentarios.js
@@ -1,4 +1,35 @@
 module.exports = function(app, swig, gestorBD) {
+
+    app.get("/comentario/borrar/:id",function (req,res) {
+        let criterio = {
+            _id : gestorBD.mongo.ObjectID(req.params.id),
+        }
+
+        if (typeof req.session.usuario == "undefined" || req.session.usuario == null){
+            res.send("Usuario no en sesión");
+        } else {
+            gestorBD.listarComentarios(criterio, function (comments) {
+                if (!comments || !comments[0]) { // comments == null ||comments[0] == null
+                    res.send("Comentario no encontrado");
+                } else {
+                    let comment = comments[0];
+                    if (comment.autor != req.session.usuario) {
+                        res.send("Usuario erroneo");
+                    } else {
+                        gestorBD.borrarComentario(criterio, function (result) {
+                            if (result) {
+                                res.send("Comentario suprimido");
+                            } else {
+                                res.send("Error");
+                            }
+                        });
+                    }
+                }
+            });
+        }
+
+    });
+
     app.post("/comentarios/:cancion_id", function(req, res){
         let comment = {
             autor : req.session.usuario,
