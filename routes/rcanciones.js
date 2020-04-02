c2af6fb35518816e6fa2f5d1dc219b12ece0c518
jesQM <jezuquema@gmail.com>
2020-04-02 23:24:06 +0200

    SDI-NodeJS-9.5- Sistema de compras

--- routes/rcanciones.js
+++ routes/rcanciones.js
@@ -143,7 +143,22 @@ module.exports = function(app, swig, gestorBD) {
                 res.redirect("/publicaciones");
             }
         });
-    })
+    });
+
+    app.get('/cancion/comprar/:id', function (req, res) {
+        let cancionId = gestorBD.mongo.ObjectID(req.params.id);
+        let compra = {
+            usuario : req.session.usuario,
+            cancionId : cancionId
+        }
+        gestorBD.insertarCompra(compra ,function(idCompra){
+            if ( idCompra == null ){
+                res.send(respuesta);
+            } else {
+                res.redirect("/compras");
+            }
+        });
+    });
 
     app.get('/cancion/:id', function(req, res) {
         let criterio = { "_id" : gestorBD.mongo.ObjectID(req.params.id) };
@@ -211,6 +226,27 @@ module.exports = function(app, swig, gestorBD) {
                         canciones : canciones
                     });
                 res.send(respuesta);
+            }
+        });
+    });
+
+
+    app.get("/compras", function(req, res) {
+        let criterio = { "usuario" : req.session.usuario };
+        gestorBD.obtenerCompras(criterio, function(compras) {
+            if (compras == null) {
+                res.send("Error al listar ");
+            } else {
+                let cancionesCompradasIds = compras.map( c => c.cancionId );
+
+                let criterio = { "_id" : {$in : cancionesCompradasIds} };
+                gestorBD.obtenerCanciones(criterio, function (canciones) {
+                    let respuesta = swig.renderFile('views/bcompras.html',
+                        {
+                            canciones : canciones
+                        });
+                    res.send(respuesta);
+                });
             }
         });
     });
